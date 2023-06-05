2c906fc1c93fb1483ed5147f6483320be4824feb
Sankot23 <s.bacot@gmail.com>
2023-06-05 15:59:12 +0200

    style:nav

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+
+include 'views/_partials/_navbar.php';
+
 ?>
 
 <!DOCTYPE html>
@@ -7,16 +10,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
-    <link rel="stylesheet" href="./css/bootstrap.css">
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/bootswatch/dist/minty/bootstrap.min.css">
 
-    <title>Médi-Centre</title>
+    <title>Centre Médical de l'Étang</title>
 
 </head>
 <body>
-    
+
+
 
 </body>
 </html>
