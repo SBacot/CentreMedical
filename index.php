9a80f68891ddb9d740752c71019848b425c550c7
Sankot23 <s.bacot@gmail.com>
2023-06-05 15:11:43 +0200

    testts

--- index.php
+++ index.php
@@ -7,14 +7,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
-    <title>Médi-Center</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link href="
+https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/cerulean/bootstrap.min.css
+" rel="stylesheet">
+    <title>Médi-Centre</title>
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js'></script>
+    <script>
+
+      document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth'
+        });
+        calendar.render();
+      });
+
+    </script>
+
 </head>
 <body>
     <div id="calendar"><h1>TEST</h1></div>
 
-<script src="../js/script.js"></script>
-<script src="../fullcalendar/core/index.global.js"></script>
-<script src="../fullcalendar/timegrid/index.global.js"></script>
+
 </body>
 </html>
